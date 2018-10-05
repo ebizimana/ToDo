b6c5d0c093d553917c49faf0c520f51b54c065df
Bizimana <ebizimana@liberty.edu>
2018-10-05 13:48:41 -0400

    The logic of ToDo List is finished the styling needs more work

---
+++ assets/js/todos.js
@@ -0,0 +1,23 @@
+//Check off specific Todos By Clicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed")
+})
+
+//Click on the X to delete a todo
+$("ul").on("click", "span", function(event){
+  event.stopPropagation(); // stop the event from bobling up
+  // Retrive the li that was clicked on
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove()
+  })
+})
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    // Grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val("")
+    // create a new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>")
+  }
+})
